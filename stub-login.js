64e3308fc7ae7af78b7d690dc49f8a0ed2c76c9c
Thynson <lanxingcan@gmail.com>
2012-06-17 21:39:05 +0800

    Fix incorrect use of setCookie

--- stub-login.js
+++ stub-login.js
@@ -4,9 +4,8 @@ var stub = require("./stub-common");
 
 var server = stub.createStubServer(function(request, response, url){
 	if (url.pathname == "/login") {
-		response.setHeader("Set-Cookie", {
-			username : "dummy",
-			identity : "dummy"});
+		response.setHeader("Set-Cookie", ["username=dummy", "identity=dummy"]);
+		response.write("{}");
 	} else if (url.pathname == "/announce") {
 		response.write("Dummy anouncement");
 	}
