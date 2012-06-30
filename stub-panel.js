177d0feb0eaf1bb2b5eaf77fb0ec84d2244174bd
Thynson <lanxingcan@gmail.com>
2012-06-30 15:52:47 +0800

    Change student to Student

--- stub-panel.js
+++ stub-panel.js
@@ -7,7 +7,13 @@ var server = stub.createStubServer(function(request, response, url){
 		response.setHeader("Set-Cookie", ["username=dummy", "identity=Student"]);
 		response.write("{}");
 	} else if (url.pathname == "/announce") {
-		response.write("Dummy anouncement");
+		response.write('["系统正在建设中!", "系统也还在调试中"]');
+	} else if (url.pathname == "/profile") {
+		response.write(JSON.stringify({ name : "形参", id : "09212035", order : 25}));
+	} else if (url.pathname == "/phase") {
+		response.write(JSON.stringify({ phase : 2 }));
+	} else if (url.pathname == "/chpasswd") {
+		response.write(JSON.stringify({}));
 	}
 	response.end();
 }, {
