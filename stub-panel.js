5d48d335bdc22e7b58a8ee88e322b23c74467099
Thynson <lanxingcan@gmail.com>
2012-07-01 12:22:04 +0800

    Update stub

--- stub-panel.js
+++ stub-panel.js
@@ -9,12 +9,121 @@ var server = stub.createStubServer(function(request, response, url){
 	} else if (url.pathname == "/announce") {
 		response.write('["系统正在建设中!", "系统也还在调试中"]');
 	} else if (url.pathname == "/profile") {
-		response.write(JSON.stringify({ name : "形参", id : "09212035", order : 25}));
+		response.write(JSON.stringify({ name : "形参", id : "09212035", order : 25, selectedSubject : "xxxx"}));
 	} else if (url.pathname == "/phase") {
 		response.write(JSON.stringify({ phase : 2 }));
-	} else if (url.pathname == "/chpasswd") {
+	} else if (url.pathname == "/subject") {
+		response.write(JSON.stringify({
+			subject : [{
+				id : "xxxx",
+				name : "毕业设计双选系统",
+				desc : "毕业设计双选系统",
+				type1 : "哈哈",
+				type2 : "hehe",
+				source : "xixi",
+				selected_by: [
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "aaa" },
+					{ realname : "bbb" }
+				],
+				professor : {
+					realname : "ddd"
+				}
+			}, {
+				id : "yyyy",
+				name : "毕业设计双选系统",
+				desc : "毕业设计双选系统",
+				type1 : "哈哈",
+				type2 : "hehe",
+				source : "xixi",
+				applied_to : {
+					realname : "ccc"
+				},
+				professor : {
+					realname : "ddd"
+				}
+			}, {
+				id : "xxxx",
+				name : "毕业设计双选系统",
+				desc : "毕业设计双选系统",
+				type1 : "哈哈",
+				type2 : "hehe",
+				source : "xixi",
+				selected_by: [ {
+					realname : "aaa"
+				}, {
+					realname : "bbb"
+				} ],
+				professor : {
+					realname : "ddd"
+				}
+			}, {
+				id : "yyyy",
+				name : "毕业设计双选系统",
+				desc : "毕业设计双选系统",
+				type1 : "哈哈",
+				type2 : "hehe",
+				source : "xixi",
+				applied_to : {
+					realname : "ccc"
+				},
+				professor : {
+					realname : "ddd"
+				}
+			}]
+		}));
+	} else {
 		response.write(JSON.stringify({}));
 	}
+	request.pipe(process.stdout);
 	response.end();
 }, {
 	basePath : "bupt-tsp",
