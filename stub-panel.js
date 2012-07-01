51b1ef64e3d9c5f7af5d8951f2ab939f58a300fc
Thynson <lanxingcan@gmail.com>
2012-07-02 00:51:37 +0800

    Update stub

--- stub-panel.js
+++ stub-panel.js
@@ -5,7 +5,7 @@ var stub = require("./stub-common");
 var server = stub.createStubServer(function(request, response, url){
 	if (url.pathname == "/login") {
 		response.setHeader("Set-Cookie", ["username=dummy", "identity=Student"]);
-		response.write("{}");
+		response.write(JSON.stringify({username : "dummy", role : "Student"}));
 	} else if (url.pathname == "/announce") {
 		response.write('["系统正在建设中!", "系统也还在调试中"]');
 	} else if (url.pathname == "/profile") {
@@ -15,111 +15,54 @@ var server = stub.createStubServer(function(request, response, url){
 	} else if (url.pathname == "/subject") {
 		response.write(JSON.stringify({
 			subject : [{
-				id : "xxxx",
-				name : "毕业设计双选系统",
-				desc : "毕业设计双选系统",
-				type1 : "哈哈",
-				type2 : "hehe",
-				source : "xixi",
-				selected_by: [
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "aaa" },
-					{ realname : "bbb" }
-				],
-				professor : {
-					realname : "ddd"
-				}
-			}, {
 				id : "yyyy",
 				name : "毕业设计双选系统",
 				desc : "毕业设计双选系统",
-				type1 : "哈哈",
-				type2 : "hehe",
-				source : "xixi",
-				applied_to : {
-					realname : "ccc"
-				},
+				type1 : 1,
+				type2 : 2,
+				source : 1,
 				professor : {
-					realname : "ddd"
+					realname : "ddd",
+					username : "dummy"
+
 				}
 			}, {
 				id : "xxxx",
-				name : "毕业设计双选系统",
-				desc : "毕业设计双选系统",
-				type1 : "哈哈",
-				type2 : "hehe",
-				source : "xixi",
-				selected_by: [ {
-					realname : "aaa"
-				}, {
-					realname : "bbb"
-				} ],
+				name : "其他毕业设计系统",
+				desc : "简单描述",
+				type1 : 2,
+				type2 : 1,
+				source : 3,
 				professor : {
-					realname : "ddd"
-				}
-			}, {
-				id : "yyyy",
-				name : "毕业设计双选系统",
-				desc : "毕业设计双选系统",
-				type1 : "哈哈",
-				type2 : "hehe",
-				source : "xixi",
-				applied_to : {
-					realname : "ccc"
-				},
-				professor : {
-					realname : "ddd"
+					realname : "ddd",
+					username : "dummy"
 				}
 			}]
 		}));
+	} else if (url.pathname == "/student") {
+		response.write(JSON.stringify({
+			student : [
+				{
+					username : "09212001",
+					realname : "当米一",
+					cls : "2009211501",
+					cls_idx : 1,
+					selected : "yyyy",
+				},
+				{
+					username : "09212002",
+					realname : "当米二",
+					cls : "2009211502",
+					cls_idx : 1,
+					selected : "yyyy",
+				}
+			]
+		}));
+
+	} else if (url.pathname == "/announce") {
+		response.write(JSON.stringify({
+			announce : "正在调试中"
+		});
 	} else {
 		response.write(JSON.stringify({}));
 	}
